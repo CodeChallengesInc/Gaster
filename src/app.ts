faf21c83cd89c77f27d903d334c243c2ef25db84
tstocker79 <stockert@gmail.com>
2020-06-19 13:24:15 -0400

    update readme and remove config endpoint

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,12 @@ import { Request, Response } from 'express';
 var express = require('express');
 var app = express();
 
-var config = require('./controllers/config');
 var board = require('./controllers/board');
 var game = require('./controllers/game');
 var test = require('./controllers/test');
 var gameStatus = require('./controllers/gameStatus');
 
-const port = 81;
+const port = 3000;
 
 app.use((req: Request, res: Response, next: any) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -18,7 +17,6 @@ app.use((req: Request, res: Response, next: any) => {
   next();
 });
 
-app.use('/config', config);
 app.use('/board', board);
 app.use('/game', game);
 app.use('/test', test);
